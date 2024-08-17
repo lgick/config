df167c06e4407df2ad8bdf8cd989f1f3abb1a733
lgick <lgick@yandex.ru>
2024-08-17 19:12:49 +0300

    removes

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -30,23 +30,16 @@ return {
         "tsserver",
         "html",
         "cssls",
-        "tailwindcss",
-        "svelte",
         "lua_ls",
         "graphql",
         "emmet_ls",
-        "prismals",
-        "pyright",
       },
     })
 
     mason_tool_installer.setup({
       ensure_installed = {
         "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint",
+        "stylua",   -- lua formatter
         "eslint_d",
       },
     })
