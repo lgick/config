78eaf8aae27e85ae22ec6f64dd244f3d190a1830
lgick <lgick@yandex.ru>
2024-08-20 11:48:16 +0300

    dressing plugin

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -10,7 +10,6 @@ return {
         typescript = { "prettier" },
         javascriptreact = { "prettier" },
         typescriptreact = { "prettier" },
-        svelte = { "prettier" },
         css = { "prettier" },
         html = { "prettier" },
         json = { "prettier" },
@@ -19,7 +18,6 @@ return {
         graphql = { "prettier" },
         liquid = { "prettier" },
         --lua = { "stylua" },
-        python = { "isort", "black" },
       },
       format_on_save = {
         lsp_fallback = true,
