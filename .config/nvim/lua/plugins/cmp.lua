b8478f8fc8f612487ce23c1dad85fadd590f854a
lgick <lgick@yandex.ru>
2026-03-27 00:57:14 +0300

    cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,5 +1,5 @@
 require("luasnip.loaders.from_vscode").lazy_load({
-  paths = { vim.fn.stdpath("config") .. "/lua/snippets" },
+  paths = { vim.fn.stdpath("config") .. "/snippets" },
 })
 
 require("lazydev").setup({
@@ -10,7 +10,9 @@ require("lazydev").setup({
 })
 
 require("blink.cmp").setup({
-  snippets = { preset = "luasnip" },
+  snippets = {
+    preset = "luasnip",
+  },
   keymap = {
     preset = "none",
     ["<CR>"] = { "select_and_accept", "fallback" },
@@ -23,6 +25,7 @@ require("blink.cmp").setup({
     ["<C-d>"] = { "scroll_documentation_down", "fallback" },
   },
   completion = {
+    -- призрачный текст
     ghost_text = {
       enabled = true,
     },
@@ -30,15 +33,22 @@ require("blink.cmp").setup({
       auto_show = true,
 
       draw = {
-        treesitter = { "lsp" },
+        -- вид выпадающего списка
         columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } },
       },
     },
-    documentation = { auto_show = true },
+    documentation = {
+      auto_show = true,
+    },
   },
   signature = { enabled = true },
   fuzzy = {
     implementation = "prefer_rust_with_warning",
+    sorts = {
+      "score", -- Primary sort: by fuzzy matching score
+      "sort_text", -- Secondary sort: by sortText field if scores are equal
+      "label", -- Tertiary sort: by label if still tied
+    },
     prebuilt_binaries = {
       download = true,
       force_version = "v1.*",
