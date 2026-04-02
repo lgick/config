82cf0b1e3627d7420bfa762eed68884f75dcfce8
lgick <lgick@yandex.ru>
2026-04-02 19:46:30 +0300

    formatting

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -1,8 +1,4 @@
 require("conform").setup({
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_format = "fallback",
-  },
   -- Определяем кастомный форматер с нужными аргументами
   formatters = {
     ["nginx-config-formatter"] = {
@@ -16,17 +12,25 @@ require("conform").setup({
   },
   formatters_by_ft = {
     lua = { "stylua" },
-    javascript = { "prettierd", stop_after_first = true },
-    javascriptreact = { "prettierd", stop_after_first = true },
-    typescript = { "prettierd", stop_after_first = true },
-    typescriptreact = { "prettierd", stop_after_first = true },
-    html = { "prettierd", stop_after_first = true },
-    css = { "prettierd", stop_after_first = true },
-    json = { "prettierd", stop_after_first = true },
-    markdown = { "prettierd", stop_after_first = true },
-    graphql = { "prettierd", stop_after_first = true },
+    javascript = { "prettierd" },
+    javascriptreact = { "prettierd" },
+    typescript = { "prettierd" },
+    typescriptreact = { "prettierd" },
+    html = { "prettierd" },
+    css = { "prettierd" },
+    json = { "prettierd" },
+    jsonc = { "prettierd" },
+    markdown = { "prettierd" },
+    graphql = { "prettierd" },
     nginx = { "nginx-config-formatter" },
   },
+  format_on_save = function()
+    if vim.g.disable_autoformat then
+      return
+    end
+
+    return { timeout_ms = 500, lsp_format = "fallback" }
+  end,
 })
 
 require("nvim-autopairs").setup()
