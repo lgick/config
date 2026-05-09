707d53396f69f510c2cc8e723e2f214ecb8c1225
lgick <lgick@yandex.ru>
2026-05-09 16:53:09 +0300

    snacks

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,12 +1,20 @@
 local snacks = require('snacks')
+local backdrop = 70
 
 snacks.setup({
   input = {
     enabled = true,
+    icon = '> ',
+    win = {
+      backdrop = backdrop,
+      width = 100,
+      row = 10,
+    },
   },
 
   picker = {
     enabled = true,
+    ui_select = true,
     sources = {
       keymaps = {
         layout = { preset = 'vscode' },
@@ -15,19 +23,41 @@ snacks.setup({
         },
       },
     },
-    layout = {
-      fullscreen = true,
+    layouts = {
+      default = {
+        fullscreen = true,
+        layout = {
+          backdrop = false,
+        },
+      },
+      vertical = {
+        fullscreen = true,
+        layout = {
+          backdrop = false,
+        },
+      },
+      vscode = {
+        fullscreen = true,
+        layout = {
+          backdrop = false,
+        },
+      },
+      select = {
+        layout = {
+          backdrop = backdrop,
+          max_width = 200,
+        },
+      },
     },
+    prompt = '> ',
     win = {
       input = {
         keys = {
           ['<ESC>'] = { 'close', mode = { 'i', 'n' } },
           ['<C-j>'] = { 'list_down', mode = { 'i', 'n' } },
           ['<C-k>'] = { 'list_up', mode = { 'i', 'n' } },
-          ['<C-n>'] = { 'preview_scroll_down', mode = { 'i', 'n' } },
-          ['<C-p>'] = { 'preview_scroll_up', mode = { 'i', 'n' } },
-          ['<C-d>'] = { 'list_bottom', mode = { 'i', 'n' } },
-          ['<C-u>'] = { 'list_top', mode = { 'i', 'n' } },
+          ['<C-u>'] = { 'preview_scroll_up', mode = { 'i', 'n' } },
+          ['<C-d>'] = { 'preview_scroll_down', mode = { 'i', 'n' } },
           ['<C-o>'] = { 'qflist', mode = { 'i', 'n' } },
           ['<C-s>'] = { 'edit_split', mode = { 'i', 'n' } },
           ['<C-i>'] = { 'edit_vsplit', mode = { 'i', 'n' } },
