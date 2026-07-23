86958dc6b5d1dcfd894fffbfd114f412f969d15c
lgick <lgick@yandex.ru>
2026-07-23 13:15:51 +0300

    nvim cursor position

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -16,7 +16,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 --------------------------------------------------------
 
 vim.api.nvim_create_autocmd('BufReadPost', {
+  group = vim.api.nvim_create_augroup('RestoreCursorGroup', { clear = true }),
   callback = function(args)
+    -- В markdown файлах не требуется сохранять позицию курсора
+    local filename = vim.api.nvim_buf_get_name(args.buf)
+    if filename:match('%.md$') or filename:match('%.markdown$') then
+      return
+    end
+
     local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
     local line_count = vim.api.nvim_buf_line_count(args.buf)
     if mark[1] > 0 and mark[1] <= line_count then
